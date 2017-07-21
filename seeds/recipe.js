fd3ebcc55e992241e7c2d73d39f2c6e1647bf21e
nwake <ngaswakefield@gmail.com>
2017-07-21 15:51:13 +1200

    more css

--- seeds/recipe.js
+++ seeds/recipe.js
@@ -11,7 +11,7 @@ exports.seed = function(knex, Promise) {
         {id: 3, name: 'Vege', ingredients: 'Tomatoes, pickles, lettuce, cheese', time: '7mins', steps_id: 1, img_url: 'https://thumbnailer.mixcloud.com/unsafe/300x300/extaudio/a/c/0/6/b8dc-41cc-4973-8c37-03a4dcc8fe1c.jpg'},
         {id: 4, name: 'Sauce', ingredients: 'Mayonnaise, tomato sauce', time: '1min', steps_id: 1, img_url: 'https://ilovenz.me/wp-content/uploads/2016/11/watties-tomato-sauce-300x300.jpg'},
         {id: 5, name: 'Buns', ingredients: 'Burger buns', time: '1min', steps_id: 1, img_url: 'http://www.completelydelicious.com/wp-content/uploads/2016/11/9178973602_f0bbbb6d84_o-1-300x300.jpg'},
-        {id: 6, name: 'Assemble', ingredients: 'The burger', time: '5', steps_id: 1, img_url: 'https://www.napoleongrills.com/Portals/0/images/Blog/1_590lb-burger-weight-300x300.jpg'}
+        {id: 6, name: 'Assemble', ingredients: 'The burger', time: '5', steps_id: 4, img_url: 'https://www.napoleongrills.com/Portals/0/images/Blog/1_590lb-burger-weight-300x300.jpg'}
       ]);
     });
 };
